be397607aa7aa1f20c3b29262967691812037890
five82 <five82@users.noreply.github.com>
2024-05-19 19:05:25 -0400

    Formatting. Adding directory cleanup.

--- encode.sh
+++ encode.sh
@@ -9,13 +9,40 @@ mkdir -p /app/videos/input \
   /app/videos/output
 
 # segment the input video
-ffmpeg -i /app/videos/input/input.mkv -c:v copy -an -map 0 -segment_time 00:03:00 -f segment -reset_timestamps 1 /app/videos/segments/%04d.mkv
+ffmpeg \
+  -i /app/videos/input/input.mkv \
+  -c:v copy \
+  -an \
+  -map 0 \
+  -segment_time 00:03:00 \
+  -f segment \
+  -reset_timestamps 1 \
+  /app/videos/segments/%04d.mkv
 
 # encode the segments
 cd /app/videos/segments
 for f in *.mkv; do
-    /app/ab-av1 auto-encode -e libsvtav1 --svt tune=0 --keyint 5s --min-vmaf 93 --preset 4 --vmaf n_threads=32:n_subsample=3 --samples 3 --enc fps_mode=passthrough --input "$f" --output /app/videos/encoded-segments/"$(basename "$f")"
+    /app/ab-av1 
+      auto-encode \
+      -e libsvtav1 \
+      --svt tune=0 \
+      --keyint 5s \
+      --min-vmaf 93 \
+      --preset 4 \
+      --vmaf n_threads=32:n_subsample=3 \
+      --samples 3 \
+      --enc fps_mode=passthrough \
+      --input "$f" \
+      --output /app/videos/encoded-segments/"$(basename "$f")"
 done
 
 # concatenate the encoded segments
-ffmpeg -f concat -safe 0 -i <(for f in /app/videos/encoded-segments/*.mkv; do echo "file '$f'"; done) -c copy /app/videos/output/output.mkv
+ffmpeg \
+  -f concat \
+  -safe 0 \
+  -i <(for f in /app/videos/encoded-segments/*.mkv; \
+  do echo "file '$f'"; done) \
+  -c copy /app/videos/output/output.mkv
+
+# cleanup
+rm -rf /app/videos/segments /app/videos/encoded-segments
